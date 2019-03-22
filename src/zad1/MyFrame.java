725724876a39ff0d0387c2c0daa3d7c2ec1d3d61
Imerlith <36335136+Imerlith@users.noreply.github.com>
2019-03-22 22:59:33 +0100

    First GUI upgrades

--- src/zad1/MyFrame.java
+++ src/zad1/MyFrame.java
@@ -11,13 +11,19 @@ class MyFrame extends JFrame {
         setTitle("Traveler service");
         JLabel topLabel  = new JLabel("Proszę podać miasto oraz kraj");
         JLabel countryLabel  = new JLabel("Kraj");
+        countryLabel.setHorizontalAlignment(SwingConstants.LEFT);
         JTextField countryTxtField = new JTextField();
-        countryTxtField.setSize(200,100);
+        countryTxtField.setPreferredSize(new Dimension(200,50));
         JTextField cityTxtField = new JTextField();
+        cityTxtField.setPreferredSize(new Dimension(200,50));
+        JButton submitBtn = new JButton("Zatwierdź");
+        submitBtn.setPreferredSize(new Dimension(200,50));
         JLabel cityLabel = new JLabel("Miasto");
         JPanel topPanel = new JPanel();
         JPanel  countryPanel= new JPanel();
         JPanel  cityPanel= new JPanel();
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.add(submitBtn);
         topPanel.add(topLabel);
         countryPanel.add(countryLabel);
         countryPanel.add(countryTxtField);
@@ -26,13 +32,14 @@ class MyFrame extends JFrame {
         topLabel.setFont(new Font("Century",Font.PLAIN,20));
         GridLayout gl = new GridLayout(4,1);
         setLayout(gl);
-      getContentPane().add(topPanel);
-      getContentPane().add(countryPanel);
-      getContentPane().add(cityPanel);
+        getContentPane().add(topPanel);
+        getContentPane().add(countryPanel);
+        getContentPane().add(cityPanel);
+        getContentPane().add(bottomPanel);
 
 
 
-
+        setResizable(false);
         setVisible(true);
     }
 }
